04ef0f8d961a271da28ef462ce942ef2d5766854
Qruim <elimentorfire@gmail.com>
2024-05-10 18:19:58 +0300

    done all 10 tasks from the lab job

--- lab2/webpack.config.js
+++ lab2/webpack.config.js
@@ -1,8 +1,11 @@
 // webpack.config.js
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
+    mode: "development",
+
     entry: {
         main: path.resolve(__dirname, "./public/src/js/index.js"),
     },
@@ -13,13 +16,20 @@ module.exports = {
     plugins: [
         new HtmlWebpackPlugin({
             title: "webpack Boilerplate",
-            template: path.resolve(__dirname, "./public/src/template.html"), //шаблон
-            filename: "index.html", // ім'я вихідного файлу
+            template: path.resolve(__dirname, "./public/src/pages/index.html"), // Fixed path
+            filename: "index.html",
         }),
         new HtmlWebpackPlugin({
             title: "webpack Boilerplate",
-            template: path.resolve(__dirname, "./public/src/pages/about.html"), //шаблон
-            filename: "about.html", // ім'я вихідного файлу
+            template: path.resolve(__dirname, "./public/src/pages/about.html"), // Fixed path
+            filename: "about.html",
         }),
+        new CleanWebpackPlugin(),
     ],
+    devServer: {
+        static: {
+            directory: path.join(__dirname, "dist"), // Serve from the 'dist' directory
+        },
+        port: 8082, // Set the port to 8080
+    },
 };
