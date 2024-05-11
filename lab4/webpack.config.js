49e32b94511357457941f40d4dc571709e9eb0ff
Qruim <elimentorfire@gmail.com>
2024-05-11 23:58:02 +0300

    initial commit to lab4

---
+++ lab4/webpack.config.js
@@ -0,0 +1,67 @@
+// webpack.config.js
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+    mode: "development",
+
+    entry: {
+        main: path.resolve(__dirname, "./public/src/js/index.js"),
+    },
+    output: {
+        path: path.resolve(__dirname, "./dist"),
+        filename: "[name].bundle.js",
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(png|jpg|gif|svg)$/,
+                use: [
+                    {
+                        loader: "file-loader",
+                        options: {
+                            name: "[name].[ext]",
+                            outputPath: "images/",
+                        },
+                    },
+                ],
+            },
+        ],
+    },
+    plugins: [
+        new CopyWebpackPlugin({
+            patterns: [
+                { from: "./public/src/assets/images", to: "images" }, // Копіювати всі файли з src/assets/images в dist/images
+            ],
+        }),
+        new HtmlWebpackPlugin({
+            title: "webpack Boilerplate",
+            template: path.resolve(__dirname, "./public/src/pages/index.html"), // Fixed path
+            filename: "index.html",
+        }),
+        new HtmlWebpackPlugin({
+            title: "webpack Boilerplate",
+            template: path.resolve(__dirname, "./public/src/pages/rozklad.html"), // Fixed path
+            filename: "rozklad.html",
+        }),
+        new HtmlWebpackPlugin({
+            title: "webpack Boilerplate",
+            template: path.resolve(__dirname, "./public/src/pages/photo.html"), // Fixed path
+            filename: "photo.html",
+        }),
+        new HtmlWebpackPlugin({
+            title: "webpack Boilerplate",
+            template: path.resolve(__dirname, "./public/src/pages/news.html"), // Fixed path
+            filename: "news.html",
+        }),
+        new CleanWebpackPlugin(),
+    ],
+    devServer: {
+        static: {
+            directory: path.join(__dirname, "dist"), // Serve from the 'dist' directory
+        },
+        port: 8082, // Set the port to 8080
+    },
+};
